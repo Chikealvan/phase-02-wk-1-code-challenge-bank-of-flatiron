5fe691702bba8a55b7c23d7e0285bf828e650b87
chikealvan <nwadoziechikealvan@gmail.com>
2023-09-15 13:00:37 +0100

    another commit

--- src/components/FilterTransaction.js
+++ src/components/FilterTransaction.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function FilterTransaction({ setTransactions }) {
+  const [inputText, setInputText] = useState("Search..");
 
-    const [inputText,setInputText] = useState("Search..")
-
-    function filterByDescription (event) {
-        event.preventDefault();
-        setInputText(event.target.value)
-        fetch("http://localhost:3000/transactions")
-        .then(res => res.json())
-        .then(transactions => {
-            setTransactions(transactions.filter((transaction) => {
-                inputText.includes(transaction.description);
-            }));
-        })
-    }
+  function filterByDescription(event) {
+    event.preventDefault();
+    setInputText(event.target.value);
+    fetch("http://localhost:3000/transactions")
+      .then((res) => res.json())
+      .then((transactions) => {
+        setTransactions(
+          transactions.filter((transaction) => {
+            inputText.includes(transaction.description);
+          })
+        );
+      });
+  }
 
   return (
     <div>
-        <h3>Filter By Description</h3>
-        <form id="form-for-filter" onSubmit={filterByDescription}>
-            <input name='filter'
-             onChange={filterByDescription}
-              value={inputText}/>
-            <button id="filter-button">Filter</button>
-        </form>
+      <h3>Filter By Description</h3>
+      <form id="form-for-filter" onSubmit={filterByDescription}>
+        <input name="filter" onChange={filterByDescription} value={inputText} />
+        <button id="filter-button">Filter</button>
+      </form>
     </div>
-  )
+  );
 }
--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,75 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function TransactionForm({ hasChange, setHasChange }) {
+  const [formData, setFormData] = useState({
+    date: "Enter Date..",
+    description: "Enter Description..",
+    category: "Enter Category..",
+    amount: 0,
+  });
 
-    const [formData, setFormData] = useState({
-        date: "Enter Date..",
-        description: "Enter Description..",
-        category: "Enter Category..",
-        amount: 0
-    })
+  function handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
 
-    function handleChange (event) {
-        setFormData ({...formData,
-            [event.target.name]: event.target.value
-        })
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (formData.amount !== 0) {
+      fetch("http://localhost:3000/transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setFormData({
+            date: "Enter Date..",
+            description: "Enter Description..",
+            category: "Enter Category..",
+            amount: 0,
+          });
+          setHasChange(() => !hasChange);
+        });
     }
-
-    function handleSubmit (event) {
-        event.preventDefault();
-        if (formData.amount !== 0) {
-            fetch ("http://localhost:3000/transactions", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(res => res.json())
-        .then(data => {
-            setFormData({
-                date: "Enter Date..",
-                description: "Enter Description..",
-                category: "Enter Category..",
-                amount: 0
-            })
-            setHasChange(() => !hasChange)
-        })
-        }
-    }
+  }
 
   return (
     <div>
-        <h3>Enter New Transaction</h3>
-        <form id="form-for-new" onSubmit={handleSubmit}>
-            <input
-             type='text'
-              name='date'
-               value={formData.date}
-                onChange={handleChange}
-                 placeholder=''/>
-            <input
-             type='text'
-              name='description'
-               value={formData.description}
-                onChange={handleChange}
-                 placeholder=''/>
-            <input
-             type='text'
-              name='category'
-               value={formData.category}
-                onChange={handleChange}
-                 placeholder=''/>
-            <input
-             type='text'
-              name='amount'
-               value={formData.amount}
-                onChange={handleChange}
-                 placeholder=''/>
-            <button id="submit" type='submit'>Add Transaction</button>
-        </form>
+      <h3>Enter New Transaction</h3>
+      <form id="form-for-new" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="date"
+          value={formData.date}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <input
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <input
+          type="text"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <input
+          type="text"
+          name="amount"
+          value={formData.amount}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <button id="submit" type="submit">
+          Add Transaction
+        </button>
+      </form>
     </div>
-  )
+  );
 }
-
